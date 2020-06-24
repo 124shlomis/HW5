4b84eb5a041dc1e3b6d7addf977d8919da95d706
Shlomi Shitrit <124shlomis@gmail.com>
2020-06-24 09:24:27 +0300

    Create compfunc.cpp compfunc.h

---
+++ compfunc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Shlomi Shitrit on 24/06/2020.
+//
+
+#include "compfunc.h"
---
+++ compfunc.h
@@ -0,0 +1,14 @@
+//
+// Created by Shlomi Shitrit on 24/06/2020.
+//
+
+#ifndef HW5_COMPFUNC_H
+#define HW5_COMPFUNC_H
+
+
+class compfunc {
+
+};
+
+
+#endif //HW5_COMPFUNC_H
